4efcc5ccca97d1615e1bbdfd84833367a4e0e339
abrorbek_ubaydullayev <abrorbek.workemail@gmail.com>
2022-07-25 06:25:54 +0500

    fixed bugs and add proxy

--- api/main.go
+++ api/main.go
@@ -3,11 +3,22 @@ package api
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Main() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	corsCfg := cors.DefaultConfig()
+	corsCfg.AllowAllOrigins = true
+	corsCfg.AllowCredentials = true
+	corsCfg.AllowHeaders = append(corsCfg.AllowHeaders, "*")
+
+	r.Use(cors.New(corsCfg))
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "Hello from server"})
